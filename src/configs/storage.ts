0e419196f4d683b3a508c2f4249fad4b407e016e
Renan <renan.fachin@hotmail.com>
2023-02-14 17:39:23 -0300

    feat: persisting user data in ASYNC storage

---
+++ src/configs/storage.ts
@@ -0,0 +1,13 @@
+const DATABASE_NAME = '@gameplay';
+
+/**
+ * @appName:colletionName
+ */
+
+const COLLECTION_USER = `${DATABASE_NAME}:user`
+const COLLECTION_APPOINTMENTS = `${DATABASE_NAME}:appointments`
+
+export {
+   COLLECTION_USER,
+   COLLECTION_APPOINTMENTS
+}
--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useState, useEffect } from 'react'
 import * as AuthSession from 'expo-auth-session'
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from '../services/apiDiscord';
 
 const { SCOPE } = process.env
@@ -8,6 +9,8 @@ const { CDN_IMAGE } = process.env
 const { CLIENT_ID } = process.env
 const { REDIRECT_URI } = process.env
 const { RESPONSE_TYPE } = process.env
+
+import { COLLECTION_USER } from '../configs/storage'
 
 type User = {
    id: string;
@@ -67,11 +70,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
             userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
 
             // Como foi modificado os valores de retorno da api, temos que desestrutura e adicionar as novas informações
-            setUser({
+
+            // PERSISTINDO OS DADOS DO USUÁRIO
+            // Criando este objeto para facilitar na hora de salvar o dado no storage e no state
+            const userData = {
                ...userInfo.data,
                firstName,
                token: params.access_token
-            })
+            }
+
+
+            await AsyncStorage.setItem(COLLECTION_USER, JSON.stringify(userData))
+            setUser(userData)
 
          }
 
@@ -83,6 +93,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
       }
    }
 
+   // buscando os dados do storage
+   async function loadUserStorageData(){
+      const isThereAUserInThestorage = await AsyncStorage.getItem(COLLECTION_USER)
+
+      if(isThereAUserInThestorage){
+         const userLogged = JSON.parse(isThereAUserInThestorage) as User
+
+         // Inserindo o token na requisição
+         api.defaults.headers.authorization = `Bearer ${userLogged.token}`
+
+         setUser(userLogged)
+      }
+   }
+   useEffect(() => {
+      loadUserStorageData()
+   },[])
 
    return (
       <AuthContext.Provider value={{ user, loading, SignIn }}>
